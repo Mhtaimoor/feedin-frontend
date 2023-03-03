caec84bc516a02b71a266087fe3067a4b8c5dedc
Muhammad Taimoor <90235270+Mhtaimoor@users.noreply.github.com>
2023-03-03 20:21:50 +0500

    Brands Page Alignment

--- src/components/brands/BrandCard.jsx
+++ src/components/brands/BrandCard.jsx
@@ -4,17 +4,17 @@ import slider1 from "../../assets/slider1.jpg";
 export default function BrandCard() {
   return (
     <>
-      <div className="BrandCard lg:px-20">
+      <div className="BrandCard px-10 lg:px-20">
         <a
           href="#"
-          class="flex flex-col items-center h-60 bg-gray-400 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-full hover:bg-gray-100 border-gray-700 bg-gray-800 hover:bg-gray-700"
+          class="flex flex-col items-center bg-gray-400 border border-gray-200 rounded-lg shadow md:flex-row md:max-w-full hover:bg-gray-100 border-gray-700 bg-gray-800 hover:bg-gray-700"
         >
           <img
-            class="object-cover w-full rounded-t-lg md:h-auto md:w-80 md:rounded-none md:rounded-l-lg ml-1"
+            class="object-cover w-full rounded-t-lg md:h-auto md:w-80 md:rounded-none md:rounded-l-lg"
             src={slider1}
             alt=""
           />
-          <div class="flex flex-col justify-between px-4 leading-normal">
+          <div class="flex flex-col justify-between p-4 leading-normal ">
             <h5 class="mb-2 text-2xl font-bold tracking-tight  text-white">
               Dominos Pizza Restaurant
             </h5>
--- src/components/brands/Brands.jsx
+++ src/components/brands/Brands.jsx
@@ -12,7 +12,7 @@ export default function Brands() {
         <h1 className="text-white text-center text-3xl font-semibold pt-10">
           Enter Your Location
         </h1>
-        <div className="px-40 py-10">
+        <div className="md:px-10 px-10 py-10">
           <select
             id="countries"
             class="bg-gray-50 border border-3 border-gray-200 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
@@ -27,7 +27,9 @@ export default function Brands() {
         </div>
       </div>
 
-      <Footer />
+      <div>
+        <Footer />
+      </div>
     </>
   );
 }
