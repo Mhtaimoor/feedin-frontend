6842faadf13f866a8f7e8bbbc0ba82c8e4a6923b
Muhammad Taimoor <90235270+Mhtaimoor@users.noreply.github.com>
2023-04-27 19:14:25 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const Base_URL = "http://localhost:5000/";
+// const Base_URL = "http://localhost:5000/";
 
-// const Base_URL = "https://feedinn-backend.herokuapp.com/";
+const Base_URL = "https://feedinn-backend.herokuapp.com/";
 
 exports.Base_URL = Base_URL;
